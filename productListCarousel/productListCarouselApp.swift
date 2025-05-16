4bf23e514bcb3c8f99db6435a75a1d48f036b24f
Aadish Jain <aadishhere@gmail.com>
2025-05-16 10:32:02 +0530

    Initial Commit

---
+++ productListCarousel/productListCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  productListCarouselApp.swift
+//  productListCarousel
+//
+//  Created by Aadish Jain on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct productListCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
